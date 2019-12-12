9e06ebc2ae38493da44f07cc1bdcbc79ad6760bf
VilijaKveine <vilija.cizaite@gmail.com>
2019-12-11 17:22:40 -0800

    Kintamieji

--- js/main.js
+++ js/main.js
@@ -1,2 +1,66 @@
 console.log('Labas rytas');
+console.log('ljdifgogl');
+console.log('-----------------');
+console.log('-----------------');
+console.log('-----------------');
+const PI=3.141592654
+console.log(PI);
+
+const vardas = 'Vilija';
+console.log(vardas);
+
+let manoAmzius = 10;
+console.log(manoAmzius);
+manoAmzius = 10;
+
+const sunsVardas = 'Rexas';
+console.log(sunsVardas);
+
+let miestas = 'Vilnius';
+console.log(miestas);
+
+let personazas = "Don' Ki Chotas";
+console.log(personazas);
+
+var kintamasis = 'x';
+console.log(kintamasis);
+
+const skaitmenys = [0, 1, 2];
+console.log(skaitmenys);
+
+const raides = []
+
+console.log('-----------------');
+console.log('-----------------');
+console.log('-----------------');
+
+const suma = 2+2;
+console.log(suma);
+
+const suma2 = manoAmzius + 7;
+console.log(suma2);
+
+const tekstas = 'a' + 'a'
+console.log(tekstas);
+
+console.log(skaitmenys);
+
+const skaitmenuSuma = skaitmenys[0] + skaitmenys[1] + skaitmenys[2];
+console.log(skaitmenuSuma);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
